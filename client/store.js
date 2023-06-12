eb416a941fd9ea9195de169533dd96e959ccf919
vkpatel007 <vkpatel007@gmail.com>
2023-06-12 19:22:18 -0400

    add toastify everywhere
    Co-authored-by: cronullarc <cronullarc@users.noreply.github.com>
    Co-authored-by: corsodr <corsodr@users.noreply.github.com>

--- client/store.js
+++ client/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './store/userSlice';
 
+// create Redux store
+// userReducer state is referred to as user
 export const store = configureStore({
   reducer: {
     user: userReducer,
